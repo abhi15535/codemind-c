36e9c32e39d3bf9c28bdd9e73e3b9440ef9fe3d8
abhi15535 <147487679+abhi15535@users.noreply.github.com>
2023-12-31 00:23:09 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x<3)     {         printf("LIGHT");     }     if(x>=3 && x<7)     {         printf("MODERATE");     }     if(x>=7)     {         printf("HEAVY");     } }

---
+++ Rain_Fall.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<3)
+    {
+        printf("LIGHT");
+    }
+    if(x>=3 && x<7)
+    {
+        printf("MODERATE");
+    }
+    if(x>=7)
+    {
+        printf("HEAVY");
+    }
+}
